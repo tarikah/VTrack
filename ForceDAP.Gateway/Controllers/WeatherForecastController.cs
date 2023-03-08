dd7ff7d81fc9092a4c29a74521a25f01603a4908
tarik59 <74472702+tarik59@users.noreply.github.com>
2023-03-08 21:22:12 +0100

    message

--- ForceDAP.Gateway/Controllers/WeatherForecastController.cs
+++ ForceDAP.Gateway/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ForceDAP.Gateway.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cooll", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
